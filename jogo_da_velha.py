4bc39601e356d6ed51d3c720bd475fcaf421477e
Cinthia Kazue Arizono <cinthiakazue@yahoo.com.br>
2023-02-02 22:53:15 -0300

    Criação das funções de jogada

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,3 +1,5 @@
+import random
+
 print("##########################################")
 print("       Bem vindo ao jogo da velha!")
 print("##########################################")
@@ -19,12 +21,38 @@ while s == False:
         print("Opção inválida")
 
 
-a = "  1  |  2  |  3  \n-----------------\n  4  |  5  |  6  \n-----------------\n  7  |  8  |  9  "
+a = "  O  |  O  |  O  \n-----------------\n  O  |  5  |  6  \n-----------------\n  7  |  8  |  9  "
 
-print("Mapa do tabuleiro")
-print(a)
-jogada = input(f"Você é {p}, faça sua primeira jogada: ")
-if(jogada in a):
-    a = a.replace(jogada, p)
-    print(a)        
+n = 0
+while True:
+    print("Mapa do tabuleiro")
+    print(a)
+    
+def jogada_player(p):
+    jogada = input(f"Você é {p}, faça sua jogada: ")
+    jogada = int(jogada)
+    ok = True
+    if(0<jogada<10 and ok == True ):
+        jogada = str(jogada)
+        if(jogada in a):
+            a = a.replace(jogada, p)
+            print(a)
+        else:
+            print("Escolha outra opção")
+            ok = False
+    else:
+        print("Opção inválida")
+        
 
+
+def jogada_bot(b):
+    ok = False
+    while ok == False:
+        j = str(random.randrange(1,10))
+        print(j)
+        if j in a:
+            a = a.replace(j, b)
+            print(a)
+            ok = True
+        else:
+            ok = False
